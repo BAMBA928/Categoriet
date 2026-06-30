1ede0a29b2444256b153a7bb1e0bb203cd551558
BAMBA928 <bambapartou42@gmail.com>
2026-06-30 23:42:45 +0000

    4 ajouter une produit a un categorie

--- categorieimper.php
+++ categorieimper.php
@@ -51,7 +51,7 @@ do {
         foreach ($categories as $categorie) {
             if (($categorie["nom"]) === $nom) {
                 $codeExiste = false;
-                echo "le code existe deja ...\n";
+                echo "ce nom existe deja ...\n";
             }
         }
     }
@@ -62,3 +62,62 @@ $categorie = [
     "code" => $nom,
     "produit" => []
 ];
+
+//4
+$catExiste = false;
+$code = readline("saisir le code :");
+foreach ($categories as $index => $categorie) {
+    if (($categorie["code"]) === $code) {
+        $catExiste = true;
+        break;
+    }
+}
+
+if (!$catExiste)
+    do {
+
+        $reference = readline("Enter  le reference :");
+        if (empty($reference)) {
+            echo "le champ est obligatoire !! \n";
+            $codeExiste = false;
+        } else {
+            foreach ($categories as $categorie) {
+                if (($categorie["produits"]["reference"]) === $reference) {
+                    $codeExiste = false;
+                    echo "cette reference existe deja !!\n";
+                }
+            }
+        }
+    } while (!$codeExiste);
+
+do {
+
+    $nom = readline("Enter  le nom du produit :");
+    if (empty($nom)) {
+        echo "le champ est obligatoire !! \n";
+        $codeExiste = false;
+    } else {
+        foreach ($categories as $categorie) {
+            if (($categorie["produits"]["nom"]) === $nom) {
+                $codeExiste = false;
+                echo "ce nom existe deja ...\n";
+            }
+        }
+    }
+} while (!$codeExiste);
+
+
+
+
+
+if ($categorieExiste) {
+    $produit = [
+        "nom" => $nom,
+        "reference" => $reference,
+        "prix" => (int) readline("Enter  le prix : "),
+        "quantite" => (int) readline("Enter la quantité : ")
+    ];
+    $categories[$index]["produits"][] = $produit;
+} else {
+    echo "cette categorie n'existe pas !!";
+}
